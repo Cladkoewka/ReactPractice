04752e2724d45fd97ca91a46d9bd95bf64956def
Cladkoewka <codewarriorms@gmail.com>
2025-01-31 21:16:55 +0300

    react router v2

--- 26-react-router/src/App.js
+++ 26-react-router/src/App.js
@@ -4,15 +4,18 @@ import Home from "./components/Home"
 import Contacts from "./components/Contacts"
 import NotFound from "./components/NotFound"
 import About from "./components/About"
+import MainLayout from "./layouts/MainLayout"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contacts" element={<Contacts />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="contacts" element={<Contacts />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
